7a5739d06ff9da38ef47a0cd258f9cd944c3829c
aniledev <elinamcgill@gmail.com>
2021-01-04 10:14:13 -0500

    CHORE: initialize project and write import/export statements

---
+++ src/app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));

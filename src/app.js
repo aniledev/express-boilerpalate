38482a690e5d49fd1189ef146f68350fb55a10dd
aniledev <elinamcgill@gmail.com>
2021-01-04 10:39:49 -0500

    CHORE: set up boilerplate for testing

--- src/app.js
+++ src/app.js
@@ -13,6 +13,18 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+// CATCH ANY THROWN ERRORS AND THEN DEFINE THE ERROR AND KEEP THE APPLICATION RUNNING
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });

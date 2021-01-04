2df08e937add32bfd590d2f29add0e298f6c2874
aniledev <elinamcgill@gmail.com>
2021-01-04 10:41:02 -0500

    CHORE: add comment blocks

--- src/app.js
+++ src/app.js
@@ -9,11 +9,18 @@ const app = express();
 
 const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
 
+//STANDARD MIDDLEWARE
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-// CATCH ANY THROWN ERRORS AND THEN DEFINE THE ERROR AND KEEP THE APPLICATION RUNNING
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// CATCH ANY THROWN ERRORS AND THEN DEFINE THE ERROR AND KEEP THE APPLICATION RUNNING;
+//STILL MIDDLEWARE
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (process.env.NODE_ENV === "production") {
@@ -25,8 +32,5 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
+//PIPELINE ENDS
 module.exports = app;

adbc322d07fa89ebad54d029b81358f06212de65
aniledev <elinamcgill@gmail.com>
2021-02-09 01:39:34 -0500

    FEAT: create boilerplate for router module

--- src/router.js
+++ src/router.js
@@ -1,1 +1,10 @@
 // This is the module that holds the router for the different endpoints.
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const service = require("./service");
+
+const Router = express.Router();
+const bodyParser = express.json();
+
+Router.route().get().post().;
